96e759f56f0d630ba7160d7c04a528db108520b7
T. Ismael Verdugo <ismael@cryptomkt.com>
2026-04-07 10:29:09 -0400

    feat: updates walletService getTransactions response

--- lib/models/response/transaction.ts
+++ lib/models/response/transaction.ts
@@ -1,16 +1,24 @@
+import { WalletTransactionSubType, WalletTransactionType } from "../enums"
+
 export interface Transaction {
   id: string
   legacy_id: string
+  reference_id: number
+  account_id: number
   currency: string
   direction: number
+  direction_display: string
   amount: number
   fee: string
   balance: number
   address: string
   hash: string
-  type: number
-  sub_type: number
+  type: WalletTransactionType
+  type_display: number
+  sub_type: WalletTransactionSubType
+  sub_type_display: number
   status: number
+  status_display: string
   created_at: string
   updated_at: string
 }
